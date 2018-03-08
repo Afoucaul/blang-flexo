7969dd3cfd856312a75c1851a0cd8021820928ca
armand.foucault@telecom-bretagne.eu <armand@armand-laptop.localhost>
2018-03-08 16:29:13 +0100

    Added sources

--- src/core/src/org/blangflexo/core/ApiAbstractor.java
+++ src/core/src/org/blangflexo/core/ApiAbstractor.java
@@ -1,17 +1,22 @@
 package org.blangflexo.core;
 
+import java.util.HashMap;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.rodinp.core.IRodinDB;
+import org.rodinp.core.IRodinFile;
 import org.rodinp.core.IRodinProject;
 import org.rodinp.core.RodinCore;
+import org.rodinp.core.basis.RodinElement;
 
 public class ApiAbstractor {
 	private static IWorkspace 	workspace	= null;
 	private static IRodinDB		rodinDB		= null;
+	private static HashMap<Pair<Class, String>, Object> environment;
 	
 	
 	private static void start() {
@@ -27,7 +32,18 @@ public class ApiAbstractor {
 		rodinDB = RodinCore.valueOf(workspace.getRoot());
 	}
 	
-	public static IRodinProject createRodinProject(final String projectName) {
+	private static void insertIntoEnvironment(Object element, String name) {
+		Pair<Class, String> key = new Pair(element.getClass(), name);
+		System.out.println("Inserting " + element.getClass() + " instance '" + name + "'");
+		environment.put(key, element);
+	}
+	
+	private static Object lookupEnvironment(Class type, String name) {
+		Pair<String, Class> key = new Pair(type, name);
+		return environment.get(key);
+	}
+	
+	public static IRodinProject createRodinProject(final String projectName) throws ApiAbstractorException {
 		try {
 			final IRodinProject rodinProject = rodinDB.getRodinProject(projectName);
 			final IProject project = rodinProject.getProject();
@@ -44,19 +60,39 @@ public class ApiAbstractor {
 					alreadyRodin = true;
 					break;
 				}
-			if (!alreadyRodin) {     // Add the Rodin nature
+			if (!alreadyRodin) {
 				final String[] newNatures = new String[natures.length + 1];
 				System.arraycopy(natures, 0, newNatures, 0, natures.length);
 				newNatures[natures.length] = RodinCore.NATURE_ID;
 				description.setNatureIds(newNatures);
 				project.setDescription(description, null);
 			}
-			project.setDerived(true);  // mark the project as derived (this could be used to distinguish user editable projects
+			project.setDerived(true); // ???
+			
+			insertIntoEnvironment(rodinProject, projectName);
 			return rodinProject;
 			
 		} catch (final CoreException ex) {
 			System.out.println("Could not create Rodin project\n\tError:" + ex);
-			return null;
+			throw new ApiAbstractorException();
 		}
 	}
+	/*
+	public static IRodinFile createRodinConstruct(final String projectName, final String fileName) {
+		RodinProject = 
+		if (rProject == null)
+			return null;
+		try {
+			final IRodinFile rodinFile = rProject.getRodinFile(filename);
+			rodinFile.create(true, null);
+			rodinFile.getResource().setDerived(true);    // mark the file as derived
+			if (rodinFile instanceof ICommentedElement)
+ 				if (comment != null && !comment.trim().equals(""))
+					((ICommentedElement) rodinFile).setComment(comment, null);
+			return rodinFile;
+		} catch (final Exception e) {
+                       .... exception code ....
+		}
+	}
+	*/
 }
---
+++ src/core/src/org/blangflexo/core/ApiAbstractorException.java
@@ -0,0 +1,10 @@
+package org.blangflexo.core;
+
+public class ApiAbstractorException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
